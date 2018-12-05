ea7d1b0e42ab3f1e82e2e48dcc3b768666182769
Carson Pavao <christinepavao@Christines-MacBook-Pro.local>
2018-12-04 19:36:18 -0500

    iwjsfl pasted

---
+++ JS-manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [ 
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+    myMusic[1] = 
+      {
+      "artist": "Muse",
+      "title": "Absolution",
+      "release_year": 2004,
+      "formats": [ 
+        "CD",
+        "8T",
+        "LP"
+      ]
+      };
